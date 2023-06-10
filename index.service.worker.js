025b27e5b7c1047a376f7eb4bcce8b5715b18052
TWS2401 <joshuabetito2401@gmail.com>
2023-06-10 23:44:48 +0100

    testing time

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1686434283|7471817890";
+const CACHE_VERSION = "1686436708|9895725648";
 const CACHE_PREFIX = "DELTATRAVELER En-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
